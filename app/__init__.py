dc0acc19d673f150be99a1ba98944f2d30e40741
KevinLiao <cooltut@hotmail.com>
2020-12-17 18:25:56 +0800

    新增后台管理模块

--- app/__init__.py
+++ app/__init__.py
@@ -4,12 +4,28 @@
     :time: 2020/12/17
     :contact: cooltut@hotmail.com
 """
-from flask import Flask
+from flask import Flask, request, session
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+from flask_babelex import Babel
+
 
 db = SQLAlchemy()
 migrate = Migrate()
+babel = Babel()
+
+
+@babel.localeselector
+def get_locale():
+    """
+    国际化支持
+    :return:
+    """
+    if request.args.get('lang'):
+        session['lang'] = request.args.get('lang')
+    return session.get('lang', 'zh_CN')
 
 
 def create_app(config_cls):
@@ -23,6 +39,15 @@ def create_app(config_cls):
 
     db.init_app(app)
     migrate.init_app(app, db)
+
+    babel.init_app(app)
+
+    # 创建后台管理模块
+    from app.models import Game, Hero, Quotation
+    _admin = Admin(app, name=u'后台管理系统', template_mode='bootstrap3')
+    _admin.add_view(ModelView(Game, db.session, name=u'游戏'))
+    _admin.add_view(ModelView(Hero, db.session, name=u'英雄角色'))
+    _admin.add_view(ModelView(Quotation, db.session, name=u'语录'))
 
     # 注册错误处理模块蓝图
     from app.errors import bp as errors_bp
--- config.py
+++ config.py
@@ -21,3 +21,5 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # flask_babelex 默认语言设置
+    BABEL_DEFAULT_LOCALE = 'zh_CN'
